18bfc516b6b0ad6197e90e859e6a85bf4c7b135a
Dimitris Georgiou <dgrgiou@gmail.com>
2019-10-20 12:04:45 +0300

    Initial commit

---
+++ DGDatePicker/DGDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  DGDatePicker.h
+//  DGDatePicker
+//
+//  Created by Dimitris Georgiou on 20/10/19.
+//  Copyright © 2019 Dimitris Georgiou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGDatePicker.
+FOUNDATION_EXPORT double DGDatePickerVersionNumber;
+
+//! Project version string for DGDatePicker.
+FOUNDATION_EXPORT const unsigned char DGDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGDatePicker/PublicHeader.h>
+
+
